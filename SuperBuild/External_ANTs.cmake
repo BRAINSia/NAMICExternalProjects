93fbd986074331d66d6abd0b4fccd98d430c428e
Kent Williams <norman-k-williams@uiowa.edu>
2013-07-31 09:48:00 -0500

    COMP: Change ANTs git_tag

--- SuperBuild/External_ANTs.cmake
+++ SuperBuild/External_ANTs.cmake
@@ -74,7 +74,7 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
 
   ### --- End Project specific additions
   set(${proj}_REPOSITORY "git://github.com/stnava/ANTs.git")
-  set(${proj}_GIT_TAG "94d63c524ad41c70ed83ddce847e5c930bc7e87f")
+  set(${proj}_GIT_TAG "7b224c10211a990f7f1c3c377a97fabd04342f68")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
