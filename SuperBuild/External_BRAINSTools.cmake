50b3e0b39483faf7762a2f80c1491710d0082833
Kent Williams <norman-k-williams@uiowa.edu>
2013-08-01 09:00:31 -0500

    COMP: More boost woes

--- SuperBuild/External_BRAINSTools.cmake
+++ SuperBuild/External_BRAINSTools.cmake
@@ -128,7 +128,7 @@ if(NOT ( DEFINED "${extProjName}_SOURCE_DIR" OR ( DEFINED "USE_SYSTEM_${extProjN
 
   ### --- End Project specific additions
   set(${proj}_REPOSITORY "${git_protocol}://github.com/BRAINSia/BRAINSTools.git")
-  set(${proj}_GIT_TAG "e395e75e28c3059ee8aa451c46c89197808de846")
+  set(${proj}_GIT_TAG "87fd2b14bd65ace5dcf590ccf12cc2e1fc033ca5")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
