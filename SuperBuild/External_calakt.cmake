09b35caf4c3fc49047b6aa6be318ee9a86e7b1c5
chaircrusher <norman-k-williams@uiowa.edu>
2013-09-20 12:26:09 -0500

    COMP: add calact and tract_querier COMP: bump DTIPrep version -- fixed a CMake warning in the DTIPrep source

--- SuperBuild/External_calakt.cmake
+++ SuperBuild/External_calakt.cmake
@@ -17,8 +17,8 @@ ProjectDependancyPush(CACHED_proj ${proj})
 # Make sure that the ExtProjName/IntProjName variables are unique globally
 # even if other External_${ExtProjName}.cmake files are sourced by
 # SlicerMacroCheckExternalProjectDependency
-set(extProjName DTIPrep) #The find_package known name
-set(proj        ${extProjName}) #This local name
+set(extProjName calakt) #The find_package known name
+set(proj        calakt) #This local name
 set(${extProjName}_REQUIRED_VERSION "")  #If a required version is necessary, then set this, else leave blank
 
 #if(${USE_SYSTEM_${extProjName}})
@@ -31,7 +31,7 @@ if(DEFINED ${extProjName}_DIR AND NOT EXISTS ${${extProjName}_DIR})
 endif()
 
 # Set dependency list
-set(${proj}_DEPENDENCIES DCMTK ITKv4 SlicerExecutionModel VTK BRAINSTools ANTs)
+set(${proj}_DEPENDENCIES "")
 #if(${PROJECT_NAME}_BUILD_DICOM_SUPPORT)
 #  list(APPEND ${proj}_DEPENDENCIES DCMTK)
 #endif()
@@ -51,55 +51,19 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
       -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
   endif()
 
-  set(BRAINSCommonLibWithANTs_OPTIONS
-    -DUSE_ANTS:BOOL=${USE_ANTs}
-    -DUSE_ANTs:BOOL=${USE_ANTs}
-    )
-  if(USE_ANTs)
-    list(APPEND BRAINSCommonLibWithANTs_OPTIONS
-      -DUSE_SYSTEM_ANTs:BOOL=ON
-      -DANTs_SOURCE_DIR:PATH=${ANTs_SOURCE_DIR}
-      -DANTs_LIBRARY_DIR:PATH=${ANTs_LIBRARY_DIR}
-      -DUSE_SYSTEM_Boost:BOOL=ON
-      -DBoost_NO_BOOST_CMAKE:BOOL=ON #Set Boost_NO_BOOST_CMAKE to ON to disable the search for boost-cmake
-      -DBoost_DIR:PATH=${BOOST_ROOT}
-      -DBOOST_ROOT:PATH=${BOOST_ROOT}
-      -DBOOST_INCLUDE_DIR:PATH=${BOOST_INCLUDE_DIR}
-      -DDTIPrepTools_SUPERBUILD:STRING=OFF
-      )
-  endif()
-
-  message("BRAINSCommonLibWithANTs_OPTIONS=${BRAINSCommonLibWithANTs_OPTIONS}")
   ### --- Project specific additions here
   set(${proj}_CMAKE_OPTIONS
       -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/${proj}-install
-      -DKWSYS_USE_MD5:BOOL=ON # Required by SlicerExecutionModel
-      -DUSE_SYSTEM_ITK:BOOL=ON
-      -DUSE_SYSTEM_VTK:BOOL=ON
-      -DUSE_SYSTEM_DCMTK:BOOL=ON
-      -DUSE_SYSTEM_SlicerExecutionModel:BOOL=ON
-      -DITK_DIR:PATH=${ITK_DIR}
-      -DVTK_DIR:PATH=${VTK_DIR}
-      -DDCMTK_DIR:PATH=${DCMTK_DIR}
-      -DGenerateCLP_DIR:PATH=${GenerateCLP_DIR}
-      -DSlicerExecutionModel_DIR:PATH=${SlicerExecutionModel_DIR}
-      -DBRAINSCommonLib_DIR:PATH=${BRAINSCommonLib_DIR}
-      -D${proj}_USE_QT:BOOL=${LOCAL_PROJECT_NAME}_USE_QT
-      -DDTIPrepTools_SUPERBUILD:STRING=OFF
-      -DBRAINSTools_SOURCE_DIR:PATH=${BRAINSTools_SOURCE_DIR}
-      ${BRAINSCommonLibWithANTs_OPTIONS}
     )
 
   ### --- End Project specific additions
-  set(${proj}_REPOSITORY https://www.nitrc.org/svn/dtiprep/trunk)
+  set(${proj}_REPOSITORY git@www.calakt.org:calakt.git)
+  set(${proj}_GIT_TAG HEAD)
   ExternalProject_Add(${proj}
-    SVN_REPOSITORY ${${proj}_REPOSITORY}
-    SVN_REVISION -r "263"
-    SVN_USERNAME slicerbot
-    SVN_PASSWORD slicer
+    GIT_REPOSITORY ${${proj}_REPOSITORY}
+    GIT_TAG ${${proj}_GIT_TAG}
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/ExternalSources/${proj}
     BINARY_DIR ${proj}-build
-    INSTALL_COMMAND ""
     LOG_CONFIGURE 0  # Wrap configure in script to ignore log output from dashboards
     LOG_BUILD     0  # Wrap build in script to to ignore log output from dashboards
     LOG_TEST      0  # Wrap test in script to to ignore log output from dashboards
--- SuperBuild/External_tract_querier.cmake
+++ SuperBuild/External_tract_querier.cmake
@@ -17,8 +17,8 @@ ProjectDependancyPush(CACHED_proj ${proj})
 # Make sure that the ExtProjName/IntProjName variables are unique globally
 # even if other External_${ExtProjName}.cmake files are sourced by
 # SlicerMacroCheckExternalProjectDependency
-set(extProjName DTIPrep) #The find_package known name
-set(proj        ${extProjName}) #This local name
+set(extProjName tract_querier) #The find_package known name
+set(proj        tract_querier) #This local name
 set(${extProjName}_REQUIRED_VERSION "")  #If a required version is necessary, then set this, else leave blank
 
 #if(${USE_SYSTEM_${extProjName}})
@@ -31,7 +31,7 @@ if(DEFINED ${extProjName}_DIR AND NOT EXISTS ${${extProjName}_DIR})
 endif()
 
 # Set dependency list
-set(${proj}_DEPENDENCIES DCMTK ITKv4 SlicerExecutionModel VTK BRAINSTools ANTs)
+set(${proj}_DEPENDENCIES "")
 #if(${PROJECT_NAME}_BUILD_DICOM_SUPPORT)
 #  list(APPEND ${proj}_DEPENDENCIES DCMTK)
 #endif()
@@ -51,65 +51,23 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
       -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
   endif()
 
-  set(BRAINSCommonLibWithANTs_OPTIONS
-    -DUSE_ANTS:BOOL=${USE_ANTs}
-    -DUSE_ANTs:BOOL=${USE_ANTs}
-    )
-  if(USE_ANTs)
-    list(APPEND BRAINSCommonLibWithANTs_OPTIONS
-      -DUSE_SYSTEM_ANTs:BOOL=ON
-      -DANTs_SOURCE_DIR:PATH=${ANTs_SOURCE_DIR}
-      -DANTs_LIBRARY_DIR:PATH=${ANTs_LIBRARY_DIR}
-      -DUSE_SYSTEM_Boost:BOOL=ON
-      -DBoost_NO_BOOST_CMAKE:BOOL=ON #Set Boost_NO_BOOST_CMAKE to ON to disable the search for boost-cmake
-      -DBoost_DIR:PATH=${BOOST_ROOT}
-      -DBOOST_ROOT:PATH=${BOOST_ROOT}
-      -DBOOST_INCLUDE_DIR:PATH=${BOOST_INCLUDE_DIR}
-      -DDTIPrepTools_SUPERBUILD:STRING=OFF
-      )
-  endif()
-
-  message("BRAINSCommonLibWithANTs_OPTIONS=${BRAINSCommonLibWithANTs_OPTIONS}")
   ### --- Project specific additions here
-  set(${proj}_CMAKE_OPTIONS
-      -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/${proj}-install
-      -DKWSYS_USE_MD5:BOOL=ON # Required by SlicerExecutionModel
-      -DUSE_SYSTEM_ITK:BOOL=ON
-      -DUSE_SYSTEM_VTK:BOOL=ON
-      -DUSE_SYSTEM_DCMTK:BOOL=ON
-      -DUSE_SYSTEM_SlicerExecutionModel:BOOL=ON
-      -DITK_DIR:PATH=${ITK_DIR}
-      -DVTK_DIR:PATH=${VTK_DIR}
-      -DDCMTK_DIR:PATH=${DCMTK_DIR}
-      -DGenerateCLP_DIR:PATH=${GenerateCLP_DIR}
-      -DSlicerExecutionModel_DIR:PATH=${SlicerExecutionModel_DIR}
-      -DBRAINSCommonLib_DIR:PATH=${BRAINSCommonLib_DIR}
-      -D${proj}_USE_QT:BOOL=${LOCAL_PROJECT_NAME}_USE_QT
-      -DDTIPrepTools_SUPERBUILD:STRING=OFF
-      -DBRAINSTools_SOURCE_DIR:PATH=${BRAINSTools_SOURCE_DIR}
-      ${BRAINSCommonLibWithANTs_OPTIONS}
-    )
 
   ### --- End Project specific additions
-  set(${proj}_REPOSITORY https://www.nitrc.org/svn/dtiprep/trunk)
+  set(${proj}_REPOSITORY "https://github.com/demianw/tract_querier.git")
+  set(${proj}_GIT_TAG "7b70c810c99a75017305545c5baa11b1c4a748b1")
   ExternalProject_Add(${proj}
-    SVN_REPOSITORY ${${proj}_REPOSITORY}
-    SVN_REVISION -r "263"
-    SVN_USERNAME slicerbot
-    SVN_PASSWORD slicer
+    GIT_REPOSITORY ${${proj}_REPOSITORY}
+    GIT_TAG ${${proj}_GIT_TAG}
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/ExternalSources/${proj}
     BINARY_DIR ${proj}-build
-    INSTALL_COMMAND ""
     LOG_CONFIGURE 0  # Wrap configure in script to ignore log output from dashboards
     LOG_BUILD     0  # Wrap build in script to to ignore log output from dashboards
     LOG_TEST      0  # Wrap test in script to to ignore log output from dashboards
     LOG_INSTALL   0  # Wrap install in script to to ignore log output from dashboards
-    ${cmakeversion_external_update} "${cmakeversion_external_update_value}"
-    CMAKE_GENERATOR ${gen}
-    CMAKE_ARGS
-      ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
-      ${COMMON_EXTERNAL_PROJECT_ARGS}
-      ${${proj}_CMAKE_OPTIONS}
+    BUILD_COMMAND ""
+    CONFIGURE_COMMAND ""
+    INSTALL_COMMAND ""
 ## We really do want to install in order to limit # of include paths INSTALL_COMMAND ""
     DEPENDS
       ${${proj}_DEPENDENCIES}
