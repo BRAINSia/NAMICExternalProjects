f085ffcae3db08e374fc2aaac466d8ac6ef7af8a
Hans Johnson <hans-johnson@uiowa.edu>
2013-11-04 08:14:44 -0600

    COMP: Fix build issues on Mac 10.9

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -142,10 +142,9 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
       ${${proj}_FFTW_ARGS}
     )
   ### --- End Project specific additions
-  ## set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
-  set(${proj}_REPOSITORY ${git_protocol}://github.com/BRAINSia/ITK.git)
-
-  set(${proj}_GIT_TAG UnusedFunction)
+  set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
+ ##set(${proj}_REPOSITORY ${git_protocol}://github.com/BRAINSia/ITK.git)
+  set(${proj}_GIT_TAG e67d21fa51ad7dc440bef098b3dc0e1d4fdc5caf)
   set(ITK_VERSION_ID ITK-4.5)
 
   ExternalProject_Add(${proj}
