9dd4036df5354ad4b45ec00c6d4ea140c1e4f1ec
Hans Johnson <hans-johnson@uiowa.edu>
2013-11-13 22:33:04 -0500

    ENH: Change smp1 to smp for cluster consistency
    Both helium and neon will have smp processing environments, so prefer to use that.

--- SuperBuild/External_BRAINSTools.cmake
+++ SuperBuild/External_BRAINSTools.cmake
@@ -143,7 +143,7 @@ if(NOT ( DEFINED "${extProjName}_SOURCE_DIR" OR ( DEFINED "USE_SYSTEM_${extProjN
 
   ### --- End Project specific additions
   set(${proj}_REPOSITORY "${git_protocol}://github.com/BRAINSia/BRAINSTools.git")
-  set(${proj}_GIT_TAG "fe9c11b9f866cb90a8aae397cb10114c3d3fa895")
+  set(${proj}_GIT_TAG "19581617e0bf2230c75cfa1c32ceb735acd0efc2")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
