9be0dcb61d96fbac9b552e3c6ddc80a2798220ec
Kent Williams <norman-k-williams@uiowa.edu>
2013-12-19 17:01:34 -0600

    COMP: update ANTs tag for warning fixes.

--- SuperBuild/External_ANTs.cmake
+++ SuperBuild/External_ANTs.cmake
@@ -72,7 +72,7 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
   ### --- End Project specific additions
   #set(${proj}_REPOSITORY "https://github.com/BRAINSia/ANTs.git")
   set(${proj}_REPOSITORY "https://github.com/stnava/ANTs.git")
-  set(${proj}_GIT_TAG "c745d71db753b9a033c3bd211f4f72b4dcfde927")
+  set(${proj}_GIT_TAG "272145e482c860be4d62b8a531bb8ef0f867d009")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
