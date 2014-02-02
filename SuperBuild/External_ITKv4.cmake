a57b24e8d5a4c50fe83f280a866ca55409885941
Hans Johnson <hans-johnson@uiowa.edu>
2014-02-02 12:41:01 -0600

    BUG: Missing transform types added for ITKv4

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -145,7 +145,7 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
   ### --- End Project specific additions
   set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
   ##set(${proj}_REPOSITORY ${git_protocol}://github.com/BRAINSia/ITK.git)
-  set(${proj}_GIT_TAG e95125fefc5b1473c76037bae0a497f4852f384a)
+  set(${proj}_GIT_TAG 0e43306b8ee033eda55df95a3e7b11d207f4c89f)
   set(ITK_VERSION_ID ITK-4.5)
 
   ExternalProject_Add(${proj}
