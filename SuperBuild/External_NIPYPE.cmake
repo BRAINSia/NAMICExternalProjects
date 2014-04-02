c5708a04507cc757d8f542e704346c0be914652b
Kent Williams <norman-k-williams@uiowa.edu>
2014-04-02 10:39:47 -0500

    COMP: add back COMMON_EXTERNAL_PROJECT_ARGS to External_XXX.cmake files

--- SuperBuild/External_NIPYPE.cmake
+++ SuperBuild/External_NIPYPE.cmake
@@ -1,33 +1,17 @@
-# Make sure this file is included only once by creating globally unique varibles
-# based on the name of this included file.
-# get_filename_component(CMAKE_CURRENT_LIST_FILENAME ${CMAKE_CURRENT_LIST_FILE} NAME_WE)
-# if(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED)
-#   return()
-# endif()
-# set(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED 1)
-
-## External_${extProjName}.cmake files can be recurisvely included,
-## and cmake variables are global, so when including sub projects it
-## is important make the extProjName and proj variables
-## appear to stay constant in one of these files.
-## Store global variables before overwriting (then restore at end of this file.)
-superbuild_stack_push(CACHED_extProjName ${extProjName})
-superbuild_stack_push(CACHED_proj ${proj})
-
 # Make sure that the ExtProjName/IntProjName variables are unique globally
 # even if other External_${ExtProjName}.cmake files are sourced by
 # ExternalProject_Include_Dependencies
-set(extProjName NIPYPE) #The find_package known name
+set(proj NIPYPE) #The find_package known name
 set(proj        NIPYPE) #This local name
-set(${extProjName}_REQUIRED_VERSION "")  #If a required version is necessary, then set this, else leave blank
+set(${proj}_REQUIRED_VERSION "")  #If a required version is necessary, then set this, else leave blank
 
-#if(${USE_SYSTEM_${extProjName}})
-#  unset(${extProjName}_DIR CACHE)
+#if(${USE_SYSTEM_${proj}})
+#  unset(${proj}_DIR CACHE)
 #endif()
 
 # Sanity checks
-if(DEFINED ${extProjName}_DIR AND NOT EXISTS ${${extProjName}_DIR})
-  message(FATAL_ERROR "${extProjName}_DIR variable is defined but corresponds to non-existing directory (${${extProjName}_DIR})")
+if(DEFINED ${proj}_DIR AND NOT EXISTS ${${proj}_DIR})
+  message(FATAL_ERROR "${proj}_DIR variable is defined but corresponds to non-existing directory (${${proj}_DIR})")
 endif()
 
 # Set dependency list
@@ -36,7 +20,7 @@ set(${proj}_DEPENDENCIES "")
 # Include dependent projects if any
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
-if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" ) )
+if(NOT ( DEFINED "USE_SYSTEM_${proj}" AND "${USE_SYSTEM_${proj}}" ) )
   #message(STATUS "${__indent}Adding project ${proj}")
 
   # Set CMake OSX variable to pass down the external project
@@ -72,18 +56,19 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
     DEPENDS
       ${${proj}_DEPENDENCIES}
   )
-  set(${extProjName}_DIR ${CMAKE_BINARY_DIR}/${proj})
+  set(${proj}_DIR ${CMAKE_BINARY_DIR}/${proj})
 else()
-  if(${USE_SYSTEM_${extProjName}})
-    find_package(${extProjName} ${${extProjName}_REQUIRED_VERSION} REQUIRED)
-    message("USING the system ${extProjName}, set ${extProjName}_DIR=${${extProjName}_DIR}")
+  if(${USE_SYSTEM_${proj}})
+    find_package(${proj} ${${proj}_REQUIRED_VERSION} REQUIRED)
+    message("USING the system ${proj}, set ${proj}_DIR=${${proj}_DIR}")
   endif()
-  # The project is provided using ${extProjName}_DIR, nevertheless since other
-  # project may depend on ${extProjName}, let's add an 'empty' one
+  # The project is provided using ${proj}_DIR, nevertheless since other
+  # project may depend on ${proj}, let's add an 'empty' one
   ExternalProject_Add_Empty(${proj} "${${proj}_DEPENDENCIES}")
 endif()
 
-list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS ${extProjName}_DIR:PATH)
+mark_as_superbuild(
+  VARS ${proj}_DIR:PATH
+  LABELS "FIND_PACKAGE"
+  )
 
-superbuild_stack_pop(CACHED_extProjName extProjName)
-superbuild_stack_pop(CACHED_proj proj)
