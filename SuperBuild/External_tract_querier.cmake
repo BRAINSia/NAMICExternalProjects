035a546db0d6ebe9e206aec1594664b90a2ab2c2
David Welch <david.m.welch@gmail.com>
2014-06-11 14:24:40 -0500

    Update External_tract_querier.cmake
    Updated to bleeding edge GIT TAG: f88f0ab0a33d19a89e7992e6cd390146e7d91e69

--- SuperBuild/External_tract_querier.cmake
+++ SuperBuild/External_tract_querier.cmake
@@ -55,7 +55,7 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
 
   ### --- End Project specific additions
   set(${proj}_REPOSITORY "https://github.com/demianw/tract_querier.git")
-  set(${proj}_GIT_TAG "d1653627a52daaadc2d8f5a01e0801a136dc6632")
+  set(${proj}_GIT_TAG "f88f0ab0a33d19a89e7992e6cd390146e7d91e69")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
