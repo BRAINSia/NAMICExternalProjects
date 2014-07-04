6387be8f6d83c87f5cbc1d9782ad74f17dd68b6d
Hans Johnson <hans-johnson@uiowa.edu>
2014-07-04 11:06:17 -0500

    COMP: Allow building without QT with VTK6.

--- SuperBuild/External_VTK.cmake
+++ SuperBuild/External_VTK.cmake
@@ -1,14 +1,8 @@
 
 set(proj VTK)
 
-option(USE_VTK_6 "Build using VTK version 6" ON)
-if(USE_VTK_6)
-  set(${proj}_REQUIRED_VERSION "6.10")  #If a required version is necessary, then set this, else leave blank
-  set(VTK_VERSION_MAJOR 6)
-else()
-  set(${proj}_REQUIRED_VERSION "5.10")  #If a required version is necessary, then set this, else leave blank
-  set(VTK_VERSION_MAJOR 5)
-endif()
+set(${proj}_REQUIRED_VERSION "6.10")  #If a required version is necessary, then set this, else leave blank
+set(VTK_VERSION_MAJOR 6)
 
 # Set dependency list
 set(${proj}_DEPENDENCIES "zlib")
@@ -62,10 +56,10 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT ${CMAKE_PROJECT_N
   if(NOT APPLE)
     list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
       #-DDESIRED_QT_VERSION:STRING=4 # Unused
-      -DVTK_USE_GUISUPPORT:BOOL=ON
-      -DVTK_USE_QVTK_QTOPENGL:BOOL=ON
-      -DVTK_USE_QT:BOOL=ON   ##VTK5
-      -DVTK_Group_Qt:BOOL=ON ##VTK6
+      -DVTK_USE_GUISUPPORT:BOOL=${${PRIMARY_PROJECT_NAME}_USE_QT}
+      -DVTK_USE_QVTK_QTOPENGL:BOOL=${${PRIMARY_PROJECT_NAME}_USE_QT}
+      -DVTK_USE_QT:BOOL=${${PRIMARY_PROJECT_NAME}_USE_QT}   ##VTK5
+      -DVTK_Group_Qt:BOOL=${${PRIMARY_PROJECT_NAME}_USE_QT} ##VTK6
       -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
       )
   else()
@@ -76,18 +70,16 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT ${CMAKE_PROJECT_N
       #-DVTK_USE_RPATH:BOOL=ON # Unused
       #-DDESIRED_QT_VERSION:STRING=4 # Unused
       -DVTK_USE_GUISUPPORT:BOOL=ON
-      -DVTK_USE_QVTK_QTOPENGL:BOOL=ON
-      -DVTK_USE_QT:BOOL=ON    ## VTK5
-      -DVTK_Group_Qt:BOOL=ON  ## VTK6
+      -DVTK_USE_QVTK_QTOPENGL:BOOL=${${PRIMARY_PROJECT_NAME}_USE_QT}
+      -DVTK_USE_QT:BOOL=${${PRIMARY_PROJECT_NAME}_USE_QT}    ## VTK5
+      -DVTK_Group_Qt:BOOL=${${PRIMARY_PROJECT_NAME}_USE_QT}  ## VTK6
       -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
       )
   endif()
 
-  if(USE_VTK_6)
-    list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
-      -DModule_vtkGUISupportQt:BOOL=ON
-      -DModule_vtkGUISupportQtOpenGL:BOOL=ON)
-  endif()
+  list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
+      -DModule_vtkGUISupportQt:BOOL=${${PRIMARY_PROJECT_NAME}_USE_QT}
+      -DModule_vtkGUISupportQtOpenGL:BOOL=${${PRIMARY_PROJECT_NAME}_USE_QT})
 
   # Disable Tk when Python wrapping is enabled
   if(${PRIMARY_PROJECT_NAME}_USE_PYTHONQT)
@@ -132,13 +124,9 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT ${CMAKE_PROJECT_N
   if(NOT DEFINED git_protocol)
     set(git_protocol "git")
   endif()
-  if(USE_VTK_6)
     #set(${proj}_REPOSITORY ${git_protocol}://vtk.org/VTK.git)
     set(${proj}_GIT_REPOSITORY "${git_protocol}://github.com/Slicer/VTK.git" CACHE STRING "Repository from which to get VTK" FORCE)
     set(${proj}_GIT_TAG "4d7abb2e0232fd8b16057a34104853c073cac4cb")
-  else()
-    message(FATAL "ERROR: Only supporting VTK6")
-  endif()
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
