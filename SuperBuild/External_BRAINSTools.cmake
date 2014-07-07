8a1eed52a3bcf4ddaee6efe84482b2a294942f52
Hans Johnson <hans-johnson@uiowa.edu>
2014-07-07 13:00:38 -0500

    ENH: Adding VtkGlue to ITK build.

--- SuperBuild/External_BRAINSTools.cmake
+++ SuperBuild/External_BRAINSTools.cmake
@@ -100,7 +100,7 @@ ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj
   # message("${proj}_CMAKE_OPTIONS=${${proj}_CMAKE_OPTIONS}")
   ### --- End Project specific additions
   set(${proj}_REPOSITORY "${git_protocol}://github.com/BRAINSia/BRAINSTools.git")
-  set(${proj}_GIT_TAG "2a35dfdc4c7efbc6562083dfe378bd33726ff827")
+  set(${proj}_GIT_TAG "testFixes")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
