b96142e0d3e7b597063be60c5da0184a88af2e5b
David Welch <david.m.welch@gmail.com>
2014-08-15 12:05:04 -0500

    BUG: Updated BRAINSTools for T2 fix with singleSession.py and segmentation workflow

--- SuperBuild/External_BRAINSTools.cmake
+++ SuperBuild/External_BRAINSTools.cmake
@@ -100,7 +100,7 @@ ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj
   # message("${proj}_CMAKE_OPTIONS=${${proj}_CMAKE_OPTIONS}")
   ### --- End Project specific additions
   set(${proj}_REPOSITORY "${git_protocol}://github.com/BRAINSia/BRAINSTools.git")
-  set(${proj}_GIT_TAG "522e43c950b4b9d886d53f74a385e65271d6647d")
+  set(${proj}_GIT_TAG "d5dccb33a5374a1777bab91a1a200c7b98f49501")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
