1f8e122299599cf6544343506d88bee866af5fad
Ali Ghayoor <ali-ghayoor@uiowa.edu>
2015-03-13 14:12:03 -0500

    ENH: Updating tag for NIPYPE package

--- SuperBuild/External_NIPYPE.cmake
+++ SuperBuild/External_NIPYPE.cmake
@@ -43,7 +43,7 @@ if(NOT ( DEFINED "USE_SYSTEM_${proj}" AND "${USE_SYSTEM_${proj}}" ) )
 
   set(${proj}_REPOSITORY "${git_protocol}://github.com/BRAINSia/nipype.git")
   #set(${proj}_GIT_TAG "NewJointFusion20150211") #Joint fusion and ANTs updates.
-  set(${proj}_GIT_TAG "NewANTS20150312") #Joint fusion and ANTs updates.
+  set(${proj}_GIT_TAG "NewANTS20150313")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
