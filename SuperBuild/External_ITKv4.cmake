a4b5f94a087638e000d9be24204806d9ddfd92c0
Hans Johnson <hans-johnson@uiowa.edu>
2015-10-29 15:54:54 -0500

    COMP: Revert ITK to buildable version.

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -34,7 +34,7 @@ if(NOT DEFINED ITK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   endif()
 
   set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
-  set(${proj}_GIT_TAG 00d39d2395d4f2dbff0fc5caef0f9e2378004d72 ) # 2015-10-29
+  set(${proj}_GIT_TAG 79f7ba4735f0ed25280b717a595cfafa13fd7d16 ) # 2015-10-29 reverted 
   set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS)
 
   if(NOT ${CMAKE_PROJECT_NAME}ITKV3_COMPATIBILITY AND CMAKE_CL_64)
