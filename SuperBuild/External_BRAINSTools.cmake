8ee90410636df93ee7664f2a1c1ebced850af5c4
Hans Johnson <hans-johnson@uiowa.edu>
2018-04-19 20:31:10 -0500

    ENH: BAW tested with nipype 1.0.2 and Python 3

--- SuperBuild/External_BRAINSTools.cmake
+++ SuperBuild/External_BRAINSTools.cmake
@@ -113,7 +113,7 @@ ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj
   # message("${proj}_CMAKE_OPTIONS=${${proj}_CMAKE_OPTIONS}")
   ### --- End Project specific additions
   set(${proj}_REPOSITORY "${git_protocol}://github.com/BRAINSia/BRAINSTools.git")
-  set(${proj}_GIT_TAG "b50a6c021e2ead75aeeb4588d67fbcc05ff178a0") # 20180416
+  set(${proj}_GIT_TAG "1c35b3a6b3eac67d821ac2a9a9ea5bcfc3ee5790") # 2080418 (BAW worked!)
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
