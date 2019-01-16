9d49f17863ac17c2fa57f8f36aed47a9083fbd63
Hans Johnson <hans.j.johnson@gmail.com>
2019-01-15 21:27:34 -0600

    ENH: Fixed OpenCV4 search path.

--- SuperBuild/External_OpenCV.cmake
+++ SuperBuild/External_OpenCV.cmake
@@ -131,7 +131,8 @@ if(NOT ( DEFINED "USE_SYSTEM_${extProjName}" AND "${USE_SYSTEM_${extProjName}}" 
     DEPENDS
       ${${proj}_DEPENDENCIES}
   )
-  set(${extProjName}_DIR ${CMAKE_BINARY_DIR}/${proj}-install/share/OpenCV/)
+  #set(${extProjName}_DIR ${CMAKE_BINARY_DIR}/${proj}-install/share/OpenCV/)
+  set(${extProjName}_DIR ${CMAKE_BINARY_DIR}/${proj}-install/lib/cmake/opencv4)
 else()
   if(${USE_SYSTEM_${extProjName}})
     if(NOT ${${extProjName}_DIR})
@@ -148,7 +149,7 @@ endif()
 
 mark_as_superbuild(
   VARS
-    ${extProjName}_DIR:FILE
+    ${extProjName}_DIR:PATH
   LABELS
      "FIND_PACKAGE"
 )
